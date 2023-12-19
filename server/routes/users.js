56ffdb0860ac09eab1c75a62787aae87e2f2dcb8
Aps2612 <apsingh1722@gmail.com>
2023-12-19 20:01:18 +0530

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { registerUser, loginUser,getDashboard,updateSlot } = require("../controllers/users")
+
+
+
+
+
+
+
+router.post('/register',registerUser)
+router.get('/dashboard',getDashboard)
+router.put('/update', updateSlot);
+router.post('/',loginUser)
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+const cookieParser = require('cookie-parser');
+
+// express app
+const app = express();
+const cors = require('cors');
+
+app.options('*', cors({
+  origin: 'https://658183961872f1121f0f2e61--heartfelt-fenglisu-8e29a3.netlify.app',
+  credentials: true,
+}));
+
+
+app.use(cors({
+  origin: "https://658183961872f1121f0f2e61--heartfelt-fenglisu-8e29a3.netlify.app",
+  credentials: true,
+})); 
+// ""
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// route
+app.use('/', userRoutes);
+
+// Set strictQuery to false
+mongoose.set('strictQuery', false);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    // listen for requests
+    const PORT = process.env.PORT || 5000; // Use the provided PORT or default to 5000
+    app.listen(PORT, () => {
+      console.log(`Connected to db & listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
